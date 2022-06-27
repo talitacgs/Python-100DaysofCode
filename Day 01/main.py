6cbaa667c4be2ccec92782e5fc428af1cd5542d9
Talita C <cgstalita@gmail.com>
2022-06-27 15:47:34 -0300

    Add exercise day 01

---
+++ Day 01/main.py
@@ -0,0 +1,11 @@
+#1. Create a greeting for your program.
+print('Hi folks')
+
+#2. Ask the user for the city that they grew up in.
+city = input("What's the city that you grey up? ")
+
+#3. Ask the user for the name of a pet.
+animal = input('What is the name of favorite pet? ')
+
+#4. Combine the name of their city and pet and show them their band name.
+print('Your band name is', city, animal)
