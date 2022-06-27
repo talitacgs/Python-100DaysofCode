d5fda6ee2847bcd878837de1c89eb0e744788545
Talita C <cgstalita@gmail.com>
2022-06-27 15:52:54 -0300

    Add exercise day 05

---
+++ Day 05/main.py
@@ -0,0 +1,23 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+for i in range(0,nr_letters):
+  letras = random.sample(letters,nr_letters)
+for j in range(0,nr_symbols):
+  simbolos = random.sample(symbols,nr_symbols)
+for k in range(0,nr_numbers):
+  numeros = random.sample(numbers,nr_numbers)
+
+senha = letras+simbolos+numeros
+senha_hard = random.sample(senha,len(senha))
+senha_hard ="".join(senha_hard)
+
+
+print('Sugestão de senha: ', senha_hard)
